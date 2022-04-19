55cb947facfe7add18df8cf6f9307c1db7e7adf6
Tonychen0227 <tony.chen@outlook.com>
2022-04-19 16:36:07 -0700

    refactor to react app

--- SmashExplorerWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SmashExplorerWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SmashExplorerWeb/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SmashExplorerWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
