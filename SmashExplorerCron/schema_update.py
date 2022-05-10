eea8a7c14b8ec27bbbb07284de517d7191afcfed
Tonychen0227 <tony.chen@outlook.com>
2022-05-09 23:54:07 -0700

    New Schema Update script

--- SmashExplorerCron/schema_update.py
+++ SmashExplorerCron/schema_update.py
@@ -15,19 +15,18 @@ if __name__ == '__main__':
 
     for event_id in event_ids:
         event_count += 1
-        sets = [x for x in list(operations.get_all_sets_from_db(event_id))]
+        try:
+            sets = [x for x in list(operations.api.get_event_sets_updated_after_timestamp(event_id, 1))]
 
-        logger.log(f"Schema upgrade progress ({event_id}): {event_count} of {total_events}, found {len(sets)} sets")
+            logger.log(f"Schema upgrade progress ({event_id}): {event_count} of {total_events}, found {len(sets)} sets")
 
-        set_count = 0
-        total_sets = len(sets)
-        for set_id in [x["id"] for x in sets]:
-            set_count += 1
-            if set_count % 50 == 0:
-                logger.log(f"Sets upgrade progress: {set_count} of {total_sets}")
-            try:
-                set_from_smash_gg = operations.api.get_set(set_id)
+            set_count = 0
+            total_sets = len(sets)
+            for set_from_smash_gg in sets:
+                set_count += 1
+                if set_count % 50 == 0:
+                    logger.log(f"Sets upgrade progress: {set_count} of {total_sets}")
                 operations.cosmos.create_set(set_from_smash_gg)
-            except:
-                logging.exception("")
-                logger.log(f"WARNING: Failed to update set {set_id}")
+        except:
+            logging.exception("")
+            logger.log(f"Failed to update sets for {event_id}")
