83b47abc5f6573da0fb33897c989b30bccf9f8ee
Tonychen0227 <tony.chen@outlook.com>
2022-05-11 00:14:02 -0700

    Minor update

--- SmashExplorerWeb/SmashExplorerWeb/Database/SmashExplorerDatabase.cs
+++ SmashExplorerWeb/SmashExplorerWeb/Database/SmashExplorerDatabase.cs
@@ -249,6 +249,31 @@ public class SmashExplorerDatabase
         return ret;
     }
 
+    private string GetStringOrdinal(int num)
+    {
+        if (num <= 0) return num.ToString();
+
+        switch (num % 100)
+        {
+            case 11:
+            case 12:
+            case 13:
+                return num + "th";
+        }
+
+        switch (num % 10)
+        {
+            case 1:
+                return num + "st";
+            case 2:
+                return num + "nd";
+            case 3:
+                return num + "rd";
+            default:
+                return num + "th";
+        }
+    }
+
     private Upset MapToUpset(Set set)
     {
         Entrant winner = set.Entrants.Where(x => x.Id == set.WinnerId).Single();
@@ -271,8 +296,13 @@ public class SmashExplorerDatabase
             };
         }
 
-        var newDisplayScore = $"{winner.Name} ({winner.InitialSeedNum} seed) {set.DetailedScore[winner.Id]}-{set.DetailedScore[loser.Id]} " +
-                              $"{loser.Name} ({loser.InitialSeedNum} seed)";
+        var newDisplayScore = $"{winner.Name} ({winner.InitialSeedNum}) {set.DetailedScore[winner.Id]}-{set.DetailedScore[loser.Id]} " +
+                              $"{loser.Name} ({loser.InitialSeedNum})";
+
+        if (set.Round < 0)
+        {
+            newDisplayScore += $" (out @ {GetStringOrdinal(set.LPlacement ?? -1)})";
+        }
 
         return new Upset()
         {
