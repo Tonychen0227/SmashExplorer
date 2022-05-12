47aa35d0acf957901a71d0cc8089e2bb054feae0
Tonychen0227 <tony.chen@outlook.com>
2022-05-11 23:17:07 -0700

    Supremacy

---
+++ SmashExplorerWeb/SmashExplorerWeb/Controllers/ZookaloozaController.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SmashExplorerWeb.Controllers
+{
+    public class ZookaloozaController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            ViewBag.Title = "I believe in Zookalooza Supremacy";
+
+            return View();
+        }
+    }
+}
