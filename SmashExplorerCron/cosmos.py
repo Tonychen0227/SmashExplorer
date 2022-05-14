e547edaa85ec1ed24d9862470efce9d69f3b2c54
Tonychen0227 <tony.chen@outlook.com>
2022-05-14 10:24:29 -0700

    Minutely Resolution

--- SmashExplorerCron/cosmos.py
+++ SmashExplorerCron/cosmos.py
@@ -64,11 +64,12 @@ class CosmosDB:
 
     def get_active_event_ids(self):
         date_now = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+        date_now_minus_3_hours = int((datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(hours=3)).timestamp())
         date_now_minus_1_week = int((datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=7)).timestamp())
         date_now_plus_1_day = int((datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=8)).timestamp())
 
         response = self.events.query_items(query=f"SELECT k.id FROM k WHERE (k.state = \"ACTIVE\" AND k.startAt > {date_now_minus_1_week})"
-                                                 f"OR (k.startAt > {date_now} and k.startAt < {date_now_plus_1_day})",
+                                                 f"OR (k.startAt > {date_now_minus_3_hours} and k.startAt < {date_now_plus_1_day})",
                                            enable_cross_partition_query=True)
         return [x["id"] for x in response]
 
