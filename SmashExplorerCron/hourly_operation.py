aef0f92c748d938f2ecc33dd6ef4d76766424ef5
Tonychen0227 <tony.chen@outlook.com>
2022-05-16 14:12:51 -0700

    Smash gg rebrand

--- SmashExplorerCron/hourly_operation.py
+++ SmashExplorerCron/hourly_operation.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     event_ids = list(operations.get_active_event_ids())
     events_size = len(event_ids)
     for event_id in event_ids:
+        events_count += 1
         logger.log(f"Hourly operation on {event_id} ({events_count} of {events_size})")
         if operations.get_and_create_event(event_id) is None:
             logger.log(f"Event {event_id} has been deleted")
