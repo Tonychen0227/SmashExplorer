32920158a1caae8190fe5126a9e8efe85a862440
Tonychen0227 <tony.chen@outlook.com>
2022-05-18 23:43:17 -0700

    Combine minutely and hourly

--- SmashExplorerCron/minutely_operation.py
+++ SmashExplorerCron/minutely_operation.py
@@ -9,12 +9,16 @@ if __name__ == '__main__':
     operations = OperationsManager(logger)
     logger.log("Starting Minutely Script")
 
-    event_count = 1
+    event_count = 0
     event_ids = list(operations.get_active_event_ids())
     events_size = len(event_ids)
     for event_id in event_ids:
-        logger.log(f"Minutely operation on {event_id} ({event_count} of {events_size})")
-        operations.update_event_sets(event_id)
         event_count += 1
+        logger.log(f"Minutely operation on {event_id} ({event_count} of {events_size})")
+        if operations.get_and_create_event(event_id) is None:
+            logger.log(f"Event {event_id} has been deleted")
+            continue
+        operations.get_and_create_entrants_for_event(event_id)
+        operations.update_event_sets(event_id)
 
     logger.log("Minutely Script Complete")
