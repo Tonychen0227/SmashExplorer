5705189e6aa53212920c58c65b657128d9e90e95
Tonychen0227 <tony.chen@outlook.com>
2022-05-23 23:42:49 -0700

    Updates

--- SmashExplorerCron/datafix.py
+++ SmashExplorerCron/datafix.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     logger.log("Starting Datafix")
 
     event_count = 0
-    event_ids = [720860]
+    event_ids = []
 
     for event_id in [str(x) for x in event_ids]:
         event = operations.cosmos.get_event(event_id)
