2d53deebfebe63a5a0c4ee0e9446c0b37716ed24
Tonychen0227 <tony.chen@outlook.com>
2022-05-23 23:31:28 -0700

    less claustrophobic

--- SmashExplorerCron/datafix.py
+++ SmashExplorerCron/datafix.py
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     logger.log("Starting Datafix")
 
     event_count = 0
-    event_ids = [715686]
+    event_ids = [720860]
 
     for event_id in [str(x) for x in event_ids]:
         event = operations.cosmos.get_event(event_id)
+        event["state"] = "ACTIVE"
         event["setsLastUpdated"] = 1
         operations.cosmos.create_event_datafix(event)
