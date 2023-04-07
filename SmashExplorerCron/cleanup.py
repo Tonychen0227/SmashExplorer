9cf137dcc1555b3d3b116c398cd5eb9b56e06d9b
Tonychen0227 <tony.chen@outlook.com>
2023-04-07 16:55:29 -0700

    Update cleanup.py

--- SmashExplorerCron/cleanup.py
+++ SmashExplorerCron/cleanup.py
@@ -16,20 +16,23 @@ if __name__ == '__main__':
     date_now = int((datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=7)).timestamp())
 
     for event_id in event_ids:
-        event = operations.api.get_event(event_id)
-        event_count += 1
-        logger.log(f"Data cleanup: {event_count} of {total_events}")
+        try:
+            event = operations.api.get_event(event_id)
+            event_count += 1
+            logger.log(f"Data cleanup: {event_count} of {total_events}")
 
-        if event is None:
+            if event is None:
+                continue
+
+            if event["state"] == "ACTIVE":
+                if event["startAt"] < date_now:
+                    num_entrants = event["numEntrants"]
+                    event_sets = list(operations.cosmos.get_event_sets(event_id))
+                    if len(event_sets) > num_entrants:
+                        logger.log(f"Event {event_id} forced complete")
+                        event["state"] = "COMPLETED_FORCED"
+                        operations.cosmos.create_event(event)
+                        continue
+        except:
+            logger.log(f"Event {event_id} saw problem...")
             continue
-
-        if event["state"] == "ACTIVE":
-            if event["startAt"] < date_now:
-                num_entrants = event["numEntrants"]
-                event_sets = list(operations.cosmos.get_event_sets(event_id))
-                if len(event_sets) > num_entrants:
-                    logger.log(f"Event {event_id} forced complete")
-                    event["state"] = "COMPLETED_FORCED"
-                    operations.cosmos.create_event(event)
-                    continue
-
