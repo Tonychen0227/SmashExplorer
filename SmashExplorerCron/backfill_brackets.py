7f06b6576ea121b3d293dd9d0d4f3cd07cffaabf
Tonychen0227 <tony.chen@outlook.com>
2023-06-21 14:30:30 -0700

    Update backfill_brackets.py

--- SmashExplorerCron/backfill_brackets.py
+++ SmashExplorerCron/backfill_brackets.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
 
         for line in f:
             if phrase in line:
-                print(line)
+                print(line[6:6])
                 break
 
     exit()
